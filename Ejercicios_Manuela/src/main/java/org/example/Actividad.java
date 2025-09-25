e157f5c770258020da67b9e3351b8f8421a06397
A613\1DAW <manuelaplanelles@gmail.com>
2025-09-25 17:18:24 +0200

    Mi primer proyecto de desarrollo.

---
+++ Ejercicios_Manuela/src/main/java/org/example/Actividad.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Actividad {
+
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("¿Cómo te llamas?: ");
+        String nombre = teclado.nextLine();
+
+        System.out.println("¿A que curso vas?: ");
+        String curso = teclado.nextLine();
+
+        System.out.println("Introduce un numero:");
+        int num1= teclado.nextByte();
+        System.out.println("Introduce otro numero:");
+        int num2= teclado.nextByte();
+
+        System.out.println ("El resultado es:" +(num1+num2));
+    }
+}
