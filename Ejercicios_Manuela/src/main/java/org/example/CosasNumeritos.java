77df87dc4c923d9b9203566c9dffa0072d735b6d
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-13 20:56:25 +0200

    explicacion toString

---
+++ Ejercicios_Manuela/src/main/java/org/example/CosasNumeritos.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.Scanner;
+public class CosasNumeritos {
+    public static void main() {
+        int num1=124;
+        int num2=212;
+
+        String num_sring=Integer.toString(num);
+        String num_izquierda=num_sring.substring(0,1);
+        int num_izquierda_int=Integer.porseInt(num_izquierda);
+        System.out.println(num_izquierda_int*100);
+    }
+}
