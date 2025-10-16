dd5cfe2dfbbbc24a451a522c90fe57ea2d9389a0
MANUELA\manue <manuelaplanelles@gmail.com>
2025-10-16 15:57:22 +0200

    reorganizar archivos para arreglar clase lob

--- Ejercicios_Manuela/src/main/java/org/example/CosasNumeritos.java
+++ Ejercicios_Manuela/src/main/java/org/example/CosasNumeritos.java
@@ -19,5 +19,52 @@ public class CosasNumeritos {
         System.out.println(num_derecha_int*num1);
 
         System.out.println(num_derecha_int*num2);
+
+        //apuntes StringsCosas
+
+            String frase = "Metanyahu le regala una paloma de oro a Trum";
+            int tamanyo=frase.length(); //para indicar cuantos caracteres
+
+            //quiero obtener cierto caracter en cierta posición
+            System.out.println(frase.charAt(35)); //que pòsicion quiero coger
+            String trozo = frase.substring(38,45); //que trozo quiero coger
+            System.out.println(trozo);
+
+            String trozo2 = frase.substring(38); //para que coja desde el inicio hasta la posicion que indiques
+            int posicion_oro = frase.indexOf("oro");
+            //int posiicion_oro2 =
+            //int posicion_oro_ultima = frase.lastIndexOf( );//dime en que posicion esta
+
+
+            //pasa pasar de minuscula a mayuscula
+            String frase_mayusculas = frase.toUpperCase();
+            System.out.println(frase_mayusculas);
+            String frase_minusculas = frase.toLowerCase();
+            System.out.println(frase_minusculas);
+
+            //para escribir sin espacios
+            String nombre = "Manuela       ";
+            System.out.println(nombre.trim() + "-" + nombre + "#");
+
+            //comparar dos textos, tu le preguntas  el contesta si o no
+            boolean iguales = nombre.equals("Manuela");
+
+            String nombre3 = "Iván       ";
+            boolean iguales2 = nombre.trim().equals("Iván"); // pàra acentos
+            System.out.println(iguales2);
+
+            String nobre_mayus = nombre.toUpperCase();
+            boolean iguales3 = nobre_mayus.trim().equals("IVÁN"); //pàra mayuscula
+
+            //para obviar mayusculas y minusculas
+            boolean iguales4 = nombre.trim().equalsIgnoreCase( "IVÁN");
+            System.out.println(iguales4);
+
+            //para remplazar cosas
+            String cambiada = frase.replace("paloma", "palOma");
+
+            //para conctaenar
+            String frsase_concatenada = frase.concat(" de EEUU");
+            System.out.println();
     }
 }
--- Ejercicios_Manuela/src/main/java/org/example/Multiplicacion3Cifras.java
+++ Ejercicios_Manuela/src/main/java/org/example/Multiplicacion3Cifras.java
@@ -2,15 +2,35 @@ import java.util.Scanner;
 
 public class Multiplicacion3Cifras {
     static void main() {
+        int num1, num2;
 
-        //preguntamos cifras
-        System.out.print("Introduce el multiplicando (3 cifras): ");
-        int num1 = sc.nextInt();
-        System.out.print("Introduce el multiplicador (3 cifras): ");
-        int num2 = sc.nextInt();
+        //validar num1
+        do {
+            System.out.print("Introduce el multiplicando (3 cifras): ");
+            num1 = sc.nextInt();
+
+            if (num1 < 0) {
+                System.out.println("No se permiten números negativos.");
+            } else if (num1 < 100 || num1 > 999) {
+                System.out.println("Debe tener exactamente 3 cifras (entre 100 y 999).");
+            }
+        } while (num1 < 100 || num1 > 999);
+
+        //validar num2
+        do {                                                        //repite el numero hasta que cumpla las condiciones
+            System.out.print("Introduce el multiplicador (3 cifras): ");
+            num2 = sc.nextInt();
+
+            if (num2 < 0) {                                         //condicion numero positivo
+                System.out.println("No se permiten números negativos.");
+            } else if (num2 < 100 || num2 > 999) {                  //condicion 3cifras
+                System.out.println("Debe tener exactamente 3 cifras (entre 100 y 999).");
+            }
+        } while (num2 < 100 || num2 > 999);
+
 
         //Descomponer cifras
-        String num_sring = Integer.toString(num2);
+        String num_sring = Integer.toString(num2); //multiplicador
 
         String num_izquierda = num_sring.substring(0, 1);
         int num_izquierda = Integer.parseInt(num_izquierda);
@@ -23,6 +43,19 @@ public class Multiplicacion3Cifras {
         String num_derecha = num_sring.substring(2, 3);
         int num_derecha = Integer.parseInt(num_derecha);
         System.out.println(num_derecha * num1);
+
+        //calcular y mostrar
+        System.out.println();
+        System.out.println(" " + num1);
+        System.out.println("x " + num2);
+        System.out.println("---------");
+
+        System.out.println( (num1 * derecha) );
+        System.out.println( (num1 * centro) + "0");
+        System.out.println( (num1 * izquierda) + "00");
+        System.out.println("---------");
+
+        System.out.println(num1 * num2);
 
     }
 }
