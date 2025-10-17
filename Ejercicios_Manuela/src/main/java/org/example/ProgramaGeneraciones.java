154958089b0f51dfc948902f179cc897e5def2c7
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-17 17:04:50 +0200

    actualizamos cmbios de modelo 1 y modelo 2

--- Ejercicios_Manuela/src/main/java/org/example/ProgramaGeneraciones.java
+++ Ejercicios_Manuela/src/main/java/org/example/ProgramaGeneraciones.java
@@ -1,10 +1,12 @@
 import java.time.LocalDateTime;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
 public class ProgramaGeneraciones {
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
 
+        //variables
+        int anyo_actual = LocalDateTime.now().getYear();
         final int ANYO_MINIMO = 1990;
         int anyo_nacimiento_int = 1;
 
@@ -14,22 +16,26 @@ public class ProgramaGeneraciones {
         System.out.println("[2]- Edad");
         System.out.println("---------------------------");
 
-        int nodo = 0
-        if (teclado.hasNextInt()) {
-            nodo = teclado.nextInt();
-        }else{
-            System.out.println("Introduce un valor valido");
-            return;
-        }
+        //desarrollamos el modul 1.
         LocalDateTime fecha = LocalDateTime.now();
         int anyo_actual = fecha.getYear();
 
         if (anyo_nacimiento_int >= ANYO_MINIMO && anyo_nacimiento_int <= anyo_actual){
             if (anyo_nacimiento_int <= 1927) {
                 System.out.println("Generacion no bautizada");
-            }else if
+            }else if{
             }
         }
 
+
+        //desarrollamos el modulo 2
+        int edad = 0;           //solicitamos la edad y comprobamos que sea formato correcto
+        System.out.println("Introduce tu edad...");
+        if (teclado.hasNextInt()) {
+            edad = teclado.nextInt();
+        }else{
+            System.out.println("Formota incorecto. no es numerico.");
+            return;
+        }
     }
 }
