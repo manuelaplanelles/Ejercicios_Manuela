84c3d873e363f3f8c891f5539ec5994b59c22bb8
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-17 15:55:11 +0200

    explicacion hasnext

---
+++ Ejercicios_Manuela/src/main/java/org/example/Ejmplohasnext.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejmplohasnext {
+    public static void main(String[] args) {
+        // Scanner teclado = new Scanner(System.in);
+        //if teclado.hasNext(){;
+        //    int numerito = teclado.nextInt();
+        //    System.out.println(numerito);
+        //}else{
+        //System.out.println("*El formato incorrecto");
+    }
+}
