1fcdec47e482a57b80d8be1850fae47d8ef7ab2b
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-20 21:20:21 +0200

    corregir ejercicios

--- Ejercicios_Manuela/src/main/java/org/example/CalculadoraSimple.java
+++ Ejercicios_Manuela/src/main/java/org/example/CalculadoraSimple.java
@@ -4,10 +4,20 @@ public class CalculadoraSimple {
     public static void main(String[] args){
         Scanner teclado = new Scanner (System.in);
         double num1 = teclado.nextDouble();
+        char signo = teclado.nextCharat()
 
         System.out.println("*****BIENVENIDO A LA CALCULADORA RÁPIDA*****");
         System.out.println("Introduce operador: " );
         num1 = teclado.nextDouble();
+
+        System.out.println("------------------------------");
+        System.out.println("[+] --> suma");
+        System.out.println("[-] --> resta");
+        System.out.println("[x] --> multiplicar");
+        System.out.println("[/] --> dividir");
+        System.out.println("[R] --> raiz cuadrada");
+        System.out.println("------------------------------");
+        System.out.println("Elige una operación: ");
 
 
     }
