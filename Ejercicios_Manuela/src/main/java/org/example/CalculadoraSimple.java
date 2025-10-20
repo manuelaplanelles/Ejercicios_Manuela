011b841a63665cc9f5b08293d9bf03f1e8125100
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-20 20:36:04 +0200

    nuevo ejercicio de calculadora simple

---
+++ Ejercicios_Manuela/src/main/java/org/example/CalculadoraSimple.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CalculadoraSimple {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner (System.in);
+        
+        double num1
+    }
+    }
