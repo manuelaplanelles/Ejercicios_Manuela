9b62fcfc9e7e5a6792c28d5a6fc48879e7edf3c5
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-21 21:13:47 +0200

    corregimos y entregamos ternario

--- Ejercicios_Manuela/src/main/java/org/example/Ternario.java
+++ Ejercicios_Manuela/src/main/java/org/example/Ternario.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 public class Ternario {
     public static void main(String[] args){
 
-        int a=1, b=2, c=3;
-        int resultado = a++ == 1 && a < b++ ? a -= b :
-                a > 0 && a == b ? a += c :
+        int a = 1, b = 2, c = 3;
+        int resultado = a++ == 1 && a < b++ ? a += c :
+                        a > 0 && a == b ? a -= b :
                         b == c ? a++ : a--;
-            System.out.println(resultado);
+
+        System.out.println("Resultado = " + resultado);
+        System.out.println("a = " + a + ", b = " + b + ", c = " + c);
         }
 }
