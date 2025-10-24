e82023ab353a597f4e744919fff9d5000fb8b227
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-24 15:48:50 +0200

    ejercicio 6 de bucles

---
+++ Ejercicios_Manuela/src/main/java/org/example/ejerciciobucle6.java
@@ -0,0 +1,10 @@
+public class ejerciciobucle6 {
+    public static void main(String[] args) {
+        int n= 7;
+        int factorial = 1;
+        for (int i = n ; i > 0 ; i++ ){
+            factorial *= i ;
+        }
+        System.out.println(factorial);
+    }
+}
