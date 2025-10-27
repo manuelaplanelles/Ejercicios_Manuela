616bcd99f3dfff01aec536120ec5d6430c319bc8
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-27 21:11:57 +0100

    ejercicio 3

--- Ejercicios_Manuela/src/main/java/org/example/ejerciciorandom3.java
+++ Ejercicios_Manuela/src/main/java/org/example/ejerciciorandom3.java
@@ -1,6 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ejerciciorandom3 {
     public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        int nummax, nummin;
 
+        Random aleaatorio = new Random();
 
+        System.out.println("Indica un rango mínimo: ");
+        nummax = teclado.nextInt();
+
+        System.out.println("Indica rango máximo: ");
+        nummin = teclado.nextInt();
+
+        System.out.println("¿Cuantas nº aleatorios quieres?:");
+        int aleatorio = teclado.nextInt();
+        for (int ){
+            if (nummin>nummax) {
+                int aleatorio = aleatorio.nextInt(nummax-nummin+1)+nummin;
+            }else{
+                System.out.println("Los numeros introducidor no son correctos");
+            }
+        }
     }
 }
