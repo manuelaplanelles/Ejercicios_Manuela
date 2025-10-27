5aadfecc848180ccc07aee863e0f3fc079e77018
MANUELA\manue <manuelaplanelles@gmail.com>
2025-10-27 22:43:15 +0100

    finaliza y prueba del ejercicio 3

--- Ejercicios_Manuela/src/main/java/org/example/ejerciciorandom3.java
+++ Ejercicios_Manuela/src/main/java/org/example/ejerciciorandom3.java
@@ -2,27 +2,43 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ejerciciorandom3 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int nummax, nummin;
+        Random generador = new Random();  // Nombre diferente
 
-        Random aleaatorio = new Random();
+        // Pedir el mínimo
+        System.out.print("Indica un rango minimo: ");
+        int nummin = teclado.nextInt();
 
-        System.out.println("Indica un rango mínimo: ");
-        nummax = teclado.nextInt();
+        // Pedir el máximo
+        System.out.print("Indica rango maximo: ");
+        int nummax = teclado.nextInt();
 
-        System.out.println("Indica rango máximo: ");
-        nummin = teclado.nextInt();
-
-        System.out.println("¿Cuantas nº aleatorios quieres?:");
-        int aleatorio = teclado.nextInt();
-
-        for (int i = 0; i > nummin; i++ ){
-            if (nummin>nummax) {
-                int aleatorio = aleatorio.nextInt(nummax-nummin+1)+nummin;
-            }else{
-                System.out.println("Los numeros introducidor no son correctos");
-            }
+        // Validar que el máximo sea mayor que el mínimo
+        if (nummin > nummax) {
+            System.out.println("ERROR: El minimo no puede ser mayor que el maximo");
+            return;  // Salir del programa
         }
+
+        // Pedir cuántos números quiere
+        System.out.print("¿Cuantos numeros aleatorios quieres?: ");
+        int cantidad = teclado.nextInt();
+
+        // Validar que sea positivo
+        if (cantidad <= 0) {
+            System.out.println("ERROR: Debe ser un numero positivo");
+            return;
+        }
+
+        // Generar y mostrar los números
+        System.out.println("Numeros generados:");
+        System.out.println("-------------------");
+
+        for (int i = 0; i < cantidad; i = i + 1) {
+            int numeroAleatorio = generador.nextInt(nummax - nummin + 1) + nummin;
+            System.out.println("Numero " + (i + 1) + ": " + numeroAleatorio);
+        }
+
+        teclado.close();
     }
 }
