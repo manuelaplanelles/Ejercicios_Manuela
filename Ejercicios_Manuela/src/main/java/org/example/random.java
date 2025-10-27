b33fe86174b1fdaae81774afdaaf0efa3f228cce
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-27 20:15:44 +0100

    explicacion numero double

--- Ejercicios_Manuela/src/main/java/org/example/random.java
+++ Ejercicios_Manuela/src/main/java/org/example/random.java
@@ -2,7 +2,12 @@ import java.util.Random;
 
 public class random {
     static void main() {
+        Random aleatorio = new Random();
+        int numero1 = aleatorio.nextInt(5)+1; //para decirle desde donde tienes que empezar le suma 1
+        System.out.println(numero1);
 
+        double numero2 = aleatorio.nextDouble()*100+1;
+        System.out.println(numero2);
 
     }
 }
