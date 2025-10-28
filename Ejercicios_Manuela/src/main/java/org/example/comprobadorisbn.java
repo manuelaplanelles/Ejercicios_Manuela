53752f54368f7867335e3a626003bc091991b518
MANUELA\manue <manuelaplanelles@gmail.com>
2025-10-28 12:29:19 +0100

    se corrige el for anidado

--- Ejercicios_Manuela/src/main/java/org/example/comprobadorisbn.java
+++ Ejercicios_Manuela/src/main/java/org/example/comprobadorisbn.java
@@ -30,29 +30,29 @@ public class comprobadorisbn {
                         System.out.print("Introduce ISBN (10 caracteres): ");
                         String isbn = teclado.nextLine();
                         String isbnMayus = isbn.toUpperCase();
+
                         // Comprobar longitud
                         if (isbnMayus.length() != 10) {
                             System.out.println("ERROR: Debe tener 10 caracteres");
-                            break;
                         }
+
                         // Calcular suma
                         int suma = 0;
-                        boolean todoCorrecto = true;
+                        boolean correcto = true;
 
                         for (int posicion = 0; posicion < 10; posicion = posicion + 1) {
                             char letra = isbnMayus.charAt(posicion);
                             int multiplicador = 10 - posicion;
-                            int valor;
+                            int valor = 0;
 
-                            // Si es X en ultima posicion
+                            // X en ultima posicion
                             if (letra == 'X' && posicion == 9) {
                                 valor = 10;
                             }
-                            // Si es X en otra posicion, error
+                            // X en otra posicion, error
                             else if (letra == 'X' && posicion != 9) {
                                 System.out.println("ERROR: X solo puede estar en la ultima posicion");
-                                todoCorrecto = false;
-                                break;
+                                correcto = false;
                             }
                             // Si es un numero del 0 al 9
                             else if (letra >= '0' && letra <= '9') {
@@ -61,74 +61,65 @@ public class comprobadorisbn {
                             // Si no es ni numero ni X, error
                             else {
                                 System.out.println("ERROR: Caracter invalido en posicion " + (posicion + 1));
-                                todoCorrecto = false;
-                                break;
+                                correcto = false;
                             }
 
                             int resultado = valor * multiplicador;
                             suma = suma + resultado;
-
                         }
 
                         // Mostrar resultado final
-                        if (todoCorrecto == true) {
-
+                        if (correcto == true) {
                             if (suma % 11 == 0) {
                                 System.out.println("ISBN VALIDO");
                             } else {
                                 System.out.println("ISBN NO VALIDO");
                             }
                         }
-                        break;
-
 
                     case 2:                                                 // CASO 2: REPARAR ISBN
                         System.out.println("--- REPARAR ISBN ---");
                         System.out.print("Introduce ISBN con caracter invalido: ");
                         String isbn2 = teclado.nextLine();
-                        String isbn2Mayus = isbn2.toUpperCase();
+                        String mayuscula = isbn2.toUpperCase();
 
                         // Comprobar longitud
-                        if (isbn2Mayus.length() != 10) {
+                        if (mayuscula.length() != 10) {
                             System.out.println("ERROR: Debe tener 10 caracteres");
-                            break;
                         }
 
                         // Buscar donde esta el caracter invalido
-                        int dondeEstaElError = -1;
+                        int caracterinvalido = -1;
 
                         for (int posicion = 0; posicion < 10; posicion = posicion + 1) {
-                            char letra = isbn2Mayus.charAt(posicion);
+                            char letra = mayuscula.charAt(posicion);
 
                             // Comprobar si es valido
-                            boolean esNumero = (letra >= '0' && letra <= '9');
-                            boolean esXAlFinal = (posicion == 9 && letra == 'X');
+                            boolean num = (letra >= '0' && letra <= '9');
+                            boolean xfinal = (posicion == 9 && letra == 'X');
 
                             // Si NO es numero Y NO es X al final, es invalido
-                            if (esNumero == false && esXAlFinal == false) {
-                                dondeEstaElError = posicion;
+                            if (num == false && xfinal == false) {
+                                caracterinvalido = posicion;
                                 System.out.println("Caracter invalido encontrado en posicion " + (posicion + 1) + ": '" + letra + "'");
-                                break;
                             }
                         }
 
                         // Si no hay nada invalido
-                        if (dondeEstaElError == -1) {
+                        if (caracterinvalido == -1) {
                             System.out.println("ERROR: No hay ningun caracter para reparar");
                             System.out.println("El ISBN parece estar completo");
-                            break;
                         }
 
                         // Calcular suma de los digitos buenos
-                        int sumaBase = 0;
+                        int suma2 = 0;
 
                         for (int posicion = 0; posicion < 10; posicion = posicion + 1) {
                             // Saltar la posicion que tiene el error
-                            if (posicion == dondeEstaElError) {
-                                continue;
+                            if (posicion == caracterinvalido) {
                             }
 
-                            char letra = isbn2Mayus.charAt(posicion);
+                            char letra = mayuscula.charAt(posicion);
                             int multiplicador = 10 - posicion;
                             int valor;
 
@@ -138,68 +129,71 @@ public class comprobadorisbn {
                                 valor = letra - '0';
                             }
 
-                            sumaBase = sumaBase + (valor * multiplicador);
+                            suma2 = suma2 + (valor * multiplicador);
                         }
 
                         // Probar numeros del 0 al 9 (o 10 si es ultima posicion)
-                        int multiplicadorDelError = 10 - dondeEstaElError;
-                        int hastaQueNumero = 9;
+                        int multiplicador = 10 - caracterinvalido;
+                        int hastanum = 9;
 
-                        if (dondeEstaElError == 9) {
-                            hastaQueNumero = 10;
+                        if (caracterinvalido == 9) {
+                            hastanum = 10;
                         }
 
-                        boolean loEncontre = false;
+                        boolean solucion = false;
+                        int digitocorrecto = -1;
 
-                        for (int numeroProbar = 0; numeroProbar <= hastaQueNumero; numeroProbar = numeroProbar + 1) {
-                            int sumaTotal = sumaBase + (numeroProbar * multiplicadorDelError);
+                        for (int prueba = 0; prueba <= hastanum; prueba = prueba + 1) {
+                            int totalsuma2 = suma2 + (prueba * multiplicador);
 
-                            if (sumaTotal % 11 == 0) {
-                                System.out.println("ISBN REPARADO");
-                                System.out.print("El digito correcto en posicion " + (dondeEstaElError + 1) + " es: ");
-
-                                if (numeroProbar == 10) {
-                                    System.out.println("X");
-                                } else {
-                                    System.out.println(numeroProbar);
-                                }
-
-                                // Construir ISBN completo
-                                String isbnReparado = "";
-                                for (int posicion = 0; posicion < 10; posicion = posicion + 1) {
-                                    if (posicion == dondeEstaElError) {
-                                        if (numeroProbar == 10) {
-                                            isbnReparado = isbnReparado + "X";
-                                        } else {
-                                            isbnReparado = isbnReparado + numeroProbar;
-                                        }
-                                    } else {
-                                        isbnReparado = isbnReparado + isbn2Mayus.charAt(posicion);
-                                    }
-                                }
-
-                                System.out.println("ISBN completo: " + isbnReparado);
-                                loEncontre = true;
-                                break;
+                            if (totalsuma2 % 11 == 0) {
+                                digitocorrecto = prueba;
+                                solucion = true;
                             }
                         }
 
-                        if (loEncontre == false) {
+                        // Mostrar resultado
+                        if (solucion == true) {
+                            System.out.println("ISBN REPARADO");
+                            System.out.print("El digito correcto en posicion " + (caracterinvalido + 1) + " es: ");
+
+                            if (digitocorrecto == 10) {
+                                System.out.println("X");
+                            } else {
+                                System.out.println(digitocorrecto);
+                            }
+
+                            // Construir ISBN completo
+                            String parte1 = mayuscula.substring(0, caracterinvalido);
+                            String parte2 = "";
+
+                            if (caracterinvalido + 1 < mayuscula.length()) {
+                                parte2 = mayuscula.substring(caracterinvalido + 1);
+                            }
+
+                            String digitoString = "";
+                            if (digitocorrecto == 10) {
+                                digitoString = "X";
+                            } else {
+                                digitoString = String.valueOf(digitocorrecto);
+                            }
+
+                            String isbnReparado = parte1 + digitoString + parte2;
+                            System.out.println("ISBN completo: " + isbnReparado);
+
+                        } else {
                             System.out.println("ERROR: No se pudo reparar el ISBN");
                         }
-                        break;
 
 
                     case 3:                                                         // CASO 3: SALIR
                         System.out.println("Gracias por usar el validador de ISBN");
                         System.out.println("Hasta pronto!");
-                        break;
 
 
-                    // OPCION INVALIDA
+                                                                                    // OPCION INVALIDA
                     default:
                         System.out.println("ERROR: Elige una opcion valida (1, 2 o 3)");
-                        break;
                 }
 
             } catch (Exception e) {
