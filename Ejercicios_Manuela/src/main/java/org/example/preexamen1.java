ed712589f4df4b2aed3d7a5916f19e7221d3d23a
A613\1DAW <manuelaplanelles@gmail.com>
2025-10-28 21:18:04 +0100

    entrada de ejercicio preexamen1

--- Ejercicios_Manuela/src/main/java/org/example/preexamen1.java
+++ Ejercicios_Manuela/src/main/java/org/example/preexamen1.java
@@ -1,5 +1,11 @@
-public class preexamen1 {
-    public static void main(String[] args){
+    import java.util.Random;
+    import java.util.Scanner;
 
+    public class preexamen1{
+
+         public static void main(String[] args) {
+             Scanner teclado = new Scanner(System.in);
+             Random aleatorio = new Random();
+
+         }
     }
-}
