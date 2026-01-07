406ecf3f5cebd562fa6aab3ed740997aac2499e2
MANUELA\manue <manuelaplanelles@gmail.com>
2026-01-07 09:45:20 +0100

    j

--- Ejercicios_Manuela/src/main/java/org/example/Main.java
+++ Ejercicios_Manuela/src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -12,4 +11,3 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
-}
